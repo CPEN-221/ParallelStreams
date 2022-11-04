c5f92b5a0f531b9d66ad41a06b34e3c3fc15ce52
Mohammad Shahrad <engshahrad@users.noreply.github.com>
2022-11-03 22:00:42 -0700

    updated variable name

--- src/Main.java
+++ src/Main.java
@@ -15,99 +15,99 @@ public class Main {
         long end = 0;
         long value = 0;
         int measurements = 100;
-        long executionTimeSumInUS;
+        long executionTimeSum;
 
         System.out.println("### Stream ###");
         //
-        executionTimeSumInUS = 0;
+        executionTimeSum = 0;
         for (int i=0; i<measurements; i++) {
             start = System.nanoTime();
             value = words.stream().count();
             end = System.nanoTime();
-            executionTimeSumInUS += (end - start);
+            executionTimeSum += (end - start);
         }
         System.out.println("Word Count: " + value);
-        System.out.println("Average Execution time (ns): " + executionTimeSumInUS/measurements);
+        System.out.println("Average Execution time (ns): " + executionTimeSum/measurements);
         System.out.println("- ");
         //
-        executionTimeSumInUS = 0;
+        executionTimeSum = 0;
         for (int i=0; i<measurements; i++) {
             start = System.nanoTime();
             value = words.stream().distinct().count();
             end = System.nanoTime();
-            executionTimeSumInUS += (end - start);
+            executionTimeSum += (end - start);
         }
         System.out.println("Distinct Word Count: " + value);
-        System.out.println("Average Execution time (ns): " + executionTimeSumInUS/measurements);
+        System.out.println("Average Execution time (ns): " + executionTimeSum/measurements);
         System.out.println("- ");
         //
-        executionTimeSumInUS = 0;
+        executionTimeSum = 0;
         for (int i=0; i<measurements; i++) {
             start = System.nanoTime();
             value = words.stream().map(word -> word.length()).distinct().count();
             end = System.nanoTime();
-            executionTimeSumInUS += (end - start);
+            executionTimeSum += (end - start);
         }
         System.out.println("Distinct Word Sizes: " + value);
-        System.out.println("Average Execution time (ns): " + executionTimeSumInUS/measurements);
+        System.out.println("Average Execution time (ns): " + executionTimeSum/measurements);
         System.out.println("- ");
         //
-        executionTimeSumInUS = 0;
+        executionTimeSum = 0;
         for (int i=0; i<measurements; i++) {
             start = System.nanoTime();
             value = words.stream().filter(word -> word.compareTo("the") == 0).count();
             end = System.nanoTime();
-            executionTimeSumInUS += (end - start);
+            executionTimeSum += (end - start);
         }
         System.out.println("Occurrences of \"the\": " + value);
-        System.out.println("Average Execution time (ns): " + executionTimeSumInUS/measurements);
+        System.out.println("Average Execution time (ns): " + executionTimeSum/measurements);
         System.out.println("- ");
 
 
         System.out.println("### Parallel Stream ###");
         //
-        executionTimeSumInUS = 0;
+        executionTimeSum = 0;
         for (int i=0; i<measurements; i++) {
             start = System.nanoTime();
             value = words.parallelStream().count();
             end = System.nanoTime();
-            executionTimeSumInUS += (end - start);
+            executionTimeSum += (end - start);
         }
         System.out.println("Word Count: " + value);
-        System.out.println("Average Execution time (ns): " + executionTimeSumInUS/measurements);
+        System.out.println("Average Execution time (ns): " + executionTimeSum/measurements);
         System.out.println("- ");
         //
-        executionTimeSumInUS = 0;
+        executionTimeSum = 0;
         for (int i=0; i<measurements; i++) {
             start = System.nanoTime();
             value = words.parallelStream().distinct().count();
             end = System.nanoTime();
-            executionTimeSumInUS += (end - start);
+            executionTimeSum += (end - start);
         }
         System.out.println("Distinct Word Count: " + value);
-        System.out.println("Average Execution time (ns): " + executionTimeSumInUS/measurements);
+        System.out.println("Average Execution time (ns): " + executionTimeSum/measurements);
         System.out.println("- ");
         //
-        executionTimeSumInUS = 0;
+        executionTimeSum = 0;
         for (int i=0; i<measurements; i++) {
             start = System.nanoTime();
             value = words.parallelStream().map(word -> word.length()).distinct().count();
             end = System.nanoTime();
-            executionTimeSumInUS += (end - start);
+            executionTimeSum += (end - start);
         }
         System.out.println("Distinct Word Sizes: " + value);
-        System.out.println("Average Execution time (ns): " + executionTimeSumInUS/measurements);
+        System.out.println("Average Execution time (ns): " + executionTimeSum/measurements);
         System.out.println("- ");
         //
-        executionTimeSumInUS = 0;
+        executionTimeSum = 0;
         for (int i=0; i<measurements; i++) {
             start = System.nanoTime();
             value = words.parallelStream().filter(word -> word.compareTo("the") == 0).count();
             end = System.nanoTime();
-            executionTimeSumInUS += (end - start);
+            executionTimeSum += (end - start);
         }
         System.out.println("Occurrences of \"the\": " + value);
-        System.out.println("Average Execution time (ns): " + executionTimeSumInUS/measurements);
+        System.out.println("Average Execution time (ns): " + executionTimeSum/measurements);
         System.out.println("- ");
     }
 }
